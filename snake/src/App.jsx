289c8bc31a8a16b2259e4e1a58eeea89d9407e02
heitzlki <kirill.heitzler@gmail.com>
2021-01-29 11:58:29 +0100

    ✨feat: components and jsx

---
+++ snake/src/App.jsx
@@ -0,0 +1,8 @@
+import Snake from './components/Snake.jsx'
+export default function App() {
+  return (
+    <>
+      <Snake />
+    </>
+  )
+}
---
+++ snake/src/components/Snake.jsx
@@ -0,0 +1,7 @@
+export default function Snake() {
+  return (
+    <>
+      <a>Test</a>
+    </>
+  )
+}
